5cdf79d7210519385a6bfcefada56edbdc234b9f
Brandon <brandonmwolfram@outlook.com>
2022-03-27 12:10:46 -0400

    deleted some stuff...

--- Backend/cockroachdbhelper3.py
+++ Backend/cockroachdbhelper3.py
@@ -79,7 +79,6 @@ def delete_users(conn):
 
 
 def connector():
-    # conn=psycopg2.connect("dbname='nifty-puma-91.defaultdb' user='muntaser' password='rootpassword' host='free-tier.gcp-us-central1.cockroachlabs.cloud' port='26257'")
     conn=psycopg2.connect(secrets['dbconn'])
     return conn
    
